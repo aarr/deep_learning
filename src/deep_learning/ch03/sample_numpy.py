40aa5b5125276f6cea16a02ff7eb7e46d6015d1a
aarr <arar0529@gmail.com>
2020-08-29 23:58:24 +0900

    edit

--- src/deep_learning/ch03/sample_numpy.py
+++ src/deep_learning/ch03/sample_numpy.py
@@ -15,3 +15,19 @@ def dot():
     result = np.dot(x, y)
     log('行列掛算 result : {0}'.format(result))
 
+def sum_axis(val_axis):
+    array = [
+        [
+            [1, 2, 3],
+            [10, 20, 30],
+            [100, 200, 300]
+        ],
+        [
+            [3, 2, 1],
+            [30, 20, 10],
+            [300, 200, 100]
+        ]
+    ]
+    log('Array : \n{0}'.format(array))
+    return np.sum(array, axis=val_axis)
+
--- tests/ch03/test_sample_numpy.py
+++ tests/ch03/test_sample_numpy.py
@@ -7,3 +7,14 @@ def test_dot():
     """行列掛け算サンプル
     """
     target.dot()
+
+def test_sum_axis():
+    # axisで指定された次元が消える
+    result_axis0 = target.sum_axis(0)
+    print("SUM axis=0 : \n{0}".format(result_axis0))
+
+    result_axis1 = target.sum_axis(1)
+    print("SUM axis=1 : \n{0}".format(result_axis1))
+
+    result_axis2 = target.sum_axis(2)
+    print("SUM axis=2 : \n{0}".format(result_axis2))
